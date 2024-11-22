bf5153d33cfd2dcc0bfa3f12cbf8fe6306881796
Eng-Rezaul-Dev <rezaulflancer@gmail.com>
2024-11-22 06:18:30 +0600

    Initial commit version 1.0.0

---
+++ js/script.js
@@ -0,0 +1,26 @@
+
+function myMsg1(){
+    let button1 = document.getElementById('myId');
+    button1.innerHTML=("I am button 1");
+}
+function myMsg2(){
+    let button2 = document.getElementById('myId');
+    button2.innerHTML=("I am button 2");
+}
+
+function myPic1(){
+    let pic1 = document.getElementById('myPicId');
+    pic1.src="./images/Masud1.png";
+}
+function myPic2(){
+    let pic2 = document.getElementById('myPicId');
+    pic2.src="./images/Mitu1.jpg";
+}
+function myPic3(){
+    let pic3 = document.getElementById('myPicId');
+    pic3.src="./images/Masud2.png";
+}
+function myPic4(){
+    let pic4 = document.getElementById('myPicId');
+    pic4.src="./images/Mitu2.jpg";
+}
